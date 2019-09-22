63f87b804280634c87b584db7c376b67830ca865
Henrique Cesar <hcesarjs@gmail.com>
2019-09-22 11:19:11 -0300

    GETs e DELETE para Animal

--- src/controller/animal.controller.js
+++ src/controller/animal.controller.js
@@ -1,6 +1,7 @@
 const db = require("../config/db.config.js")
 const Animal = db.animal;
 const Cliente = db.cliente;
+const Pessoa = db.pessoa;
 
 exports.criarAnimal = async function(req, res) {
     const idcliente = req.body.idcliente;
@@ -32,5 +33,49 @@ exports.editarAnimal = async function(req, res) {
         }
     } catch (err) {
         return res.status(500).send(err);
+    }
+}
+exports.getAnimalById = async function(req, res) {
+    const { id } = req.params;
+    try {
+        const animal = await Animal.findByPk(id, {
+            attributes: { exclude: ['id'] },
+            include: [
+                { model: Cliente,
+                attributes: ['avaliacao'],
+                include: [Pessoa] }
+            ]
+        });
+        return animal ? res.status(200).send(animal) :
+        res.status(404).send("Animal não encontrado");      
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Não foi possível recuperar o animal.");
+    }
+}
+exports.getAnimalByIdCliente = async (req, res) => {
+    const { idCliente } = req.params;
+    try {
+        const animais = await Animal.findAll(
+            {
+                where : { idcliente : idCliente },
+                attributes: { exclude: ['idcliente'] }
+            }
+        )
+        return res.status(200).send(animais);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Não foi possível listar os animais do cliente.");
+    }
+}
+exports.deleteAnimal = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Animal.destroy({
+            where: { id : id }
+        })
+        return res.status(200).send("O animal foi removido.");
+    } catch (error) {
+        return res.status(500).send("Não foi possível remover o animal.");
     }
 }
--- src/route/animal.route.js
+++ src/route/animal.route.js
@@ -7,4 +7,10 @@ router.post("/cadastrarAnimal", animal.criarAnimal);
 
 router.put("/editarAnimal/:idanimal", animal.editarAnimal);
 
+router.get("/cliente/:idCliente", animal.getAnimalByIdCliente);
+
+router.get("/:id", animal.getAnimalById);
+
+router.delete("/:id", animal.deleteAnimal);
+
 module.exports = router;
