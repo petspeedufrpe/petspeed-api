31acfeb98cc3f6e315520be598062bd787ceea49
Henrique Cesar <hcesarjs@gmail.com>
2019-09-26 20:06:37 -0300

    Mudança na variavel de ambiente

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,7 +3,7 @@ console.log(process.env);
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST,
 	dialect: process.env.DB_DIALECT,
-	port: process.env.PORT,
+	port: process.env.DB_PORT,
 	timezone: "-03:00"
 });
 
