4126a2d86caeef2bef0fdc94d5e75653e4abe222
Henrique Cesar <hcesarjs@gmail.com>
2019-09-26 20:00:21 -0300

    Removido warning DEPRECATION

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
+console.log(process.env);
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST,
 	dialect: process.env.DB_DIALECT,
-	operatorsAliases: false,
 	port: process.env.PORT,
 	timezone: "-03:00"
 });
