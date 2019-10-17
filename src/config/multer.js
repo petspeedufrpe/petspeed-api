ba474e974da4aa46a116da2b0979f2236e4685d5
Caio Farias <caiofariaspe@gmail.com>
2019-10-17 19:11:45 -0300

    sei mais n

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,56 +1,36 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
 
-const storageS3 = new aws.S3({
-    accessKeyId:'AKIAZA47P5KYWWC2CDVF',
-    secretAccessKey:'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'
-});
-
-const storageTypes = {
-    local: multer.diskStorage({
-        destination: (req,file,cb) => {
-            cb(null,path.resolve(__dirname,"..","..","tmp","uploads"));
+module.exports = {
+    dest: path.resolve(__dirname,"..","..",'tmp','uploads'),
+    storage: multer.diskStorage({
+        destination: (req,file,cb)=> { 
+            cb(null,path.resolve(__dirname,"..","..",'tmp','uploads'))
         },
         filename: (req,file,cb) => {
-            crypto.randomBytes(16,(err,hash)=>{
+            crypto.randomBytes(16,(err,hash)=> {
                 if(err) cb(err);
-                file.key = `${hash.toString('hex')}-${file.originalname}`
-                cb(null,fileName);
-            });
-        }
-    }),
-    s3: multerS3({
-        s3: new aws.S3(),
-        bucket:'uploadpetspeed',
-        contentType:multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
-        key: (req,file,cb)=> {
-            crypto.randomBytes(16,(err,hash)=>{
-                if(err) cb(err)
-                const fileName = `${hash.toString('hex')}-${file.originalname}`
-                cb(null,fileName);
-            });
-        }
-    }),
-}
-module.exports = {
-    dest: path.resolve(__dirname, '..','..','tmp','uploads'),
-    
-    storage: storageTypes["s3"],
+                const fileName = `${hash.toString('hex')}-${file.originalname}`; 
 
-    fileFilter: (req,file,cb) => {
+                cb(null,fileName);
+            });
+        },
+    }),
+    limits: {
+        fileSize: 2 * 1024 * 1024,
+    },
+    fileFilter = (req,file,cb) => {
         const allowedMimes = [
-            'images/jpeg',
-            'images/pjpeg',
+            'image/jpeg',
+            'image/pjpeg',
             'image/png'
         ];
-        if(allowedMimes.includes(file.mimeType)){
-            cb(null,true);
-        } else{
-            new Error('Invalid File Type')
+
+        if(allowedMimes.includes(file.mimeType)){ 
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid File Type'));
         }
-    },
-}
+    }
+};
--- src/route/usuario.route.js
+++ src/route/usuario.route.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const usuario = require("../controller/usuario.controller.js");
 
+const Usuario = require("../model/Usuario");
+
 router.post("/cadastrarUsuario", usuario.criarUsuario);
 
 router.put("/alterarSenha", usuario.alterarSenha);
@@ -12,6 +14,26 @@ router.get("/encontrarUsuarioPorId/:idUsuario", usuario.encontrarUsuarioPorId);
 router.delete('/deletarUsuarioPorId/:idUsuario', usuario.deletarUsuarioPorId);
 
 router.post("/login", usuario.login);
+
+router.post("/posts",multer(multerConfig).single("file"), async (req,res)=>{
+    console.log(req.file);
+    const {emailUsuario:email} = req.body;
+    try{
+        const usuario = Usuario.findOne({
+            where:{
+                email
+            }
+        });
+        if(usuario){
+            usuario.update(
+                { senha: senhaNova },
+                { where: { email } }
+            );
+            res.json("Foto Salva Com Sucesso");
+        }
+    } catch(err){}
+    return res.json({message: "Teste"});
+})
 
 router.post("/cadastrarCliente", usuario.cadastrarCliente);
 
