91304aeddfbd10ba304da6e8d60599cb0e7f603c
Caio Farias <caiofariaspe@gmail.com>
2019-10-17 17:58:39 -0300

    multer COnfig

--- src/route/pessoa.route.js
+++ src/route/pessoa.route.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
+const multer = require("multer");
+const multerConfig = require("../config/multer");
 var verifyJWT = require("../config/user.auth.js");
 const pessoa = require("../controller/pessoa.controller.js");
 
@@ -8,7 +9,10 @@ router.post("/cadastrarPessoa", pessoa.criarPessoa);
 
 router.put("/editarPessoa/:idpessoa", pessoa.editarPessoa);
 
-
+router.post("/upload",multer(multerConfig).single("file"), (req,res)=> {
+    console.log(req.file)
+    return res.json({hello:'Teste'});
+});
 router.post("/cadastrarEndereco", pessoa.dbInsert);
 
 router.get("/:id", pessoa.findByPk);
