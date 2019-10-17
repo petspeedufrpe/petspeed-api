b6355b66a463f8890af00e1dfe8e088c24003b42
Ricardo Castro <ricardo.henriquecastro@ufrpe.br>
2019-10-17 10:38:35 -0300

    Update pessoa.controller.js

--- src/controller/pessoa.controller.js
+++ src/controller/pessoa.controller.js
@@ -74,7 +74,7 @@ exports.findByPk = async function(req, res) {
 exports.findByIdUsuario = async function(req, res) {
   idusuario = req.params.idusuario;
   try {
-    const pessoa = await Pessoa.findOne({where: {idusuario: idusuario}})
+    const pessoa = await Pessoa.findOne({where: {idusuario: idusuario}, include:[Usuario]})
     if (pessoa) {
       return res.status(200).send(pessoa)
     }
